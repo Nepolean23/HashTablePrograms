87d7278ae099e7b0b4cb555bc20af0c3fd0d582c
Nepolean23 <nlean1996@gmail.com>
2023-01-22 19:25:18 +0530

    [NEPOLEAN]-Creating a simple HashTableLinkedList

---
+++ HashTableProgram/AbilityToFindFrequencyOfWords.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableProgram
+{
+    internal class AbilityToFindFrequencyOfWords<K,V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+        public AbilityToFindFrequencyOfWords(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+
+        
+
+        public struct KeyValue<k, v>
+        {
+            public k Key { get; set; }
+            public v Value { get; set; }
+        }
+
+
+
+
+    }
+
+}
