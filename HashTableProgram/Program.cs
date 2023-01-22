d70aa900679faad47c98a474fbb31f6411b30537
Nepolean23 <nlean1996@gmail.com>
2023-01-22 19:39:40 +0530

    [NEPOLEAN]-Code For Finding The IndexOfThe Words In HashTable Program

--- HashTableProgram/Program.cs
+++ HashTableProgram/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using HashTableProgram;
+
+Console.WriteLine("Hash table demo"); //() []
+ToFindIndexOfTheWords<string, string> hash = new ToFindIndexOfTheWords<string, string>(5);
+hash.Add("0", "To");
+hash.Add("1", "be");
+hash.Add("2", "or");
+hash.Add("3", "not");
+hash.Add("4", "to");
+hash.Add("5", "be");
+string hash5 = hash.Get("5");
+Console.WriteLine("5th index value: " + hash5);
+string hash2 = hash.Get("2");
+Console.WriteLine("2th index value: " + hash2);
+Console.ReadKey();
